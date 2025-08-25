8cd6343e2031a39d41aca89e0378a7e14c364892
ankush-github-11 <ankush10yt@gmail.com>
2025-08-25 23:59:21 +0530

    Updating the Backend and Sending Response

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import requests
 app = Flask(__name__)
 @app.route("/", methods=['POST'])
@@ -7,11 +7,11 @@ def index():
     amount = data["queryResult"]["parameters"]["unit-currency"]["amount"]
     source_currency = data["queryResult"]["parameters"]["unit-currency"]["currency"]
     target_currency = data["queryResult"]["parameters"]["currency-name"]
-    print(amount)
-    print(source_currency)
-    print(target_currency)
-    print(fetchCurrencyFactor(amount, source_currency, target_currency))
-    return "<h1>Hello</h1>"
+    target_amount = round(fetchCurrencyFactor(amount, source_currency, target_currency), 2)
+    response = {
+        'fulfillmentText': "{} {} is {} {}".format(amount, source_currency, target_amount, target_currency)
+    }
+    return jsonify(response)
 def fetchCurrencyFactor(amount, source_currency, target_currency):
     url = f"https://v6.exchangerate-api.com/v6/897b6b5ee1e617cf3161e3e7/latest/{source_currency}"
     res = requests.get(url)
@@ -19,11 +19,8 @@ def fetchCurrencyFactor(amount, source_currency, target_currency):
     if res.status_code == 200:
         currencyJSON = res.json()
         rate = currencyJSON["conversion_rates"].get(target_currency)
-        if rate:
-            return amount * rate
-        else:
-            return None
-    else:
-        return None
+        if rate: return amount * rate
+        else: return None
+    else: return None
 if __name__ == "__main__":
     app.run(debug=True)
